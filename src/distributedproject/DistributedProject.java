3e32e1c7edd947397286bcaf9406c20f3fc69ffc
GDLMadushanka <gdlmadushanka@gmail.com>
2017-01-05 19:50:56 +0530

    initial

---
+++ src/distributedproject/DistributedProject.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributedproject;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ *
+ * @author Lahiru
+ */
+public class DistributedProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ControlPanel cp = new ControlPanel();
+       cp.setVisible(true);
+    }
+    
+}
